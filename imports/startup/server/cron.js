1955c732c03b876ce2d3efbe8ad041b38c0e92ca
Hugh Willson <hugh@octonary.com>
2016-09-08 05:32:03 -0400

    Adjusted to use cron npm package.

--- imports/startup/server/cron.js
+++ imports/startup/server/cron.js
@@ -1,20 +1,13 @@
 import { Meteor } from 'meteor/meteor';
-import { SyncedCron } from 'meteor/percolate:synced-cron';
-
+import { CronJob } from 'cron';
 import DropboxRedisSynch from '../../api/files/server/dropbox_redis_synch';
 
 // Always run once when the server first starts
 DropboxRedisSynch.run();
 
-if (Meteor.settings.private.cronEnabled) {
-  SyncedCron.add({
-    name: 'Dropbox -> Redis Synch',
-    schedule(parser) {
-      return parser.text('every 5 minutes');
-    },
-    job() {
-      DropboxRedisSynch.run();
-    },
-  });
-  SyncedCron.start();
+if (Meteor.settings.private.cron.enabled) {
+  const job = new CronJob(Meteor.settings.private.cron.schedule, () => {
+    DropboxRedisSynch.run();
+  }, null, false, 'America/Los_Angeles');
+  job.start();
 }
--- imports/startup/server/register_api.js
+++ imports/startup/server/register_api.js
@@ -1,5 +1,2 @@
 import '../../api/files/methods';
 // import '../../api/files/server/publications';
-
-// TODO - temporary; will eventually be imported via a cron entry
-import '../../api/files/server/dropbox_redis_synch';
